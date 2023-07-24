be09e4f42335056f8c858a433347a45928fd9a1d
hara0510 <oqj0510@gmail.com>
2023-07-24 22:29:45 +0900

    Active Storageの導入

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,12 @@ class Message < ApplicationRecord
 
   belongs_to :room
   belongs_to :user
+  has_one_attached :image
+
+  private
+
+  def message_parfams
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 
 end
